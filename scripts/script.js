3633ffcc50e929073a6a546b9e6696023dfa6c6f
Don Christensen <noobgrammer@gmail.com>
2023-09-23 06:36:24 -0700

    converted handrail to a class

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,73 @@
 "use strict";
 
 const lengthInput = document.querySelector("#dimension-input");
-const picketList = document.querySelector(".pickets-list");
+// const picketList = document.querySelector(".pickets-list");
 const btn = document.querySelector(".btn");
 const form = document.querySelector(".form-rate");
 const handRails = document.querySelector(".handrail-section");
+const stairSection = document.querySelector(".stairs-section");
+const inputLabel = document.querySelector("#base-input-label"); //FIXME this needs a different name, not using label
 
-function calculatePickets(dimension) {
-  let picketMaxSpace = 5.5;
-  let picketThickness = 1.5;
+let tempChoice; //FIXME this is a temp global to determine what choice was made
+//for now I need it global until I refactor
 
-  let pickets = Math.ceil((dimension - picketThickness) / picketMaxSpace);
-  renderPickets((dimension - picketThickness) / pickets, pickets);
-}
-function renderPickets(spacing, picket) {
-  let nextPicket = spacing;
-  for (let i = 0; i < picket; i++) {
-    let picketLI = document.createElement("li");
-    picketLI.textContent = nextPicket.toFixed(3);
-    nextPicket += spacing;
-    console.log(picketLI.textContent);
-    picketList.append(picketLI);
+// function calculatePickets(dimension) {
+//   let picketMaxSpace = 5.5;
+//   let picketThickness = 1.5;
+
+//   let pickets = Math.ceil((dimension - picketThickness) / picketMaxSpace);
+//   renderPickets((dimension - picketThickness) / pickets, pickets);
+// }
+//FIXME give preCheck a better name, doesn't make much sense.
+function preCheck(choice) {
+  if (choice.innerHTML !== "") {
+    choice.innerHTML = "";
+  }
+  if (!document.querySelector(".invalid").classList.contains("hidden")) {
+    document.querySelector(".invalid").classList.add("hidden");
   }
 }
+// function renderPickets(spacing, picket) {
+//   let nextPicket = spacing;
+//   for (let i = 0; i < picket; i++) {
+//     let picketLI = document.createElement("li");
+//     picketLI.textContent = nextPicket.toFixed(3);
+//     nextPicket += spacing;
+//     console.log(picketLI.textContent);
+//     picketList.append(picketLI);
+//   }
+// }
+function radioChoice(dimensions) {
+  console.log(tempChoice); //TODO debugging
+  if (tempChoice === "stairs") {
+    const stair = new Stairs(dimensions);
+    preCheck(stair.stairOl);
+    inputLabel.textContent = "Enter overall height";
+    stair.stairMath();
+  } else if (tempChoice === "handRail") {
+    const handrail = new Handrail(dimensions);
+    preCheck(handrail.picketList);
 
+    inputLabel.textContent = "Enter length of handrail";
+    handrail.calculatePickets();
+  } else if (tempChoice === "roof") {
+    //do this
+  }
+}
+function inputBreakdown() {
+  const checkDimension = lengthInput.value;
+  if (isNaN(checkDimension)) {
+    const usableDimension = breakingInputDown(checkDimension);
+    if (isNaN(usableDimension)) {
+      document.querySelector(".invalid").classList.remove("hidden");
+    } else {
+      radioChoice(+usableDimension);
+    }
+  } else {
+    radioChoice(+checkDimension);
+  }
+}
+//FIXME this needs to be set up to not care what choice I do just break input down to usable numbers
 function evaluateInput() {
   const picketDimension = lengthInput.value;
   console.log(picketDimension);
@@ -51,62 +95,61 @@ function evaluateInput() {
     stair.stairMath(+picketDimension);
     calculatePickets(+picketDimension);
   }
-
-  function breakingInputDown(dismantling) {
-    let foot = 0;
-    let inch = 0;
-    let numerator = 0;
-    let denominator = 1;
-    let footMark = 0;
-    let inchMark = 0;
-    let divMark = 0;
-    let length = 0;
-
-    if (dismantling.indexOf("'") !== -1) {
-      footMark = dismantling.indexOf("'");
-      foot = +dismantling.slice(0, footMark);
-      length = foot * 12;
-    }
-    if (dismantling.indexOf('"') !== -1) {
-      inchMark = dismantling.indexOf('"');
-      inch = +dismantling.slice(footMark + 1, inchMark);
-      length += inch;
-    }
-    if (dismantling.indexOf("/") !== -1) {
-      divMark = dismantling.indexOf("/");
-      numerator = +dismantling.slice(inchMark + 1, divMark);
-      denominator = +dismantling.slice(divMark + 1);
-      if (denominator > 0) {
-        length += numerator / denominator;
-      }
-    }
-    console.log(
-      `foot is ${foot} inch ${inch} num ${numerator} and den is ${denominator}`
-    );
-    console.log(length);
-    return length;
-  }
 }
+function breakingInputDown(dismantling) {
+  let foot = 0;
+  let inch = 0;
+  let numerator = 0;
+  let denominator = 1;
+  let footMark = 0;
+  let inchMark = 0;
+  let divMark = 0;
+  let length = 0;
+
+  if (dismantling.indexOf("'") !== -1) {
+    footMark = dismantling.indexOf("'");
+    foot = +dismantling.slice(0, footMark);
+    length = foot * 12;
+  }
+  if (dismantling.indexOf('"') !== -1) {
+    inchMark = dismantling.indexOf('"');
+    inch = +dismantling.slice(footMark + 1, inchMark);
+    length += inch;
+  }
+  if (dismantling.indexOf("/") !== -1) {
+    divMark = dismantling.indexOf("/");
+    numerator = +dismantling.slice(inchMark + 1, divMark);
+    denominator = +dismantling.slice(divMark + 1);
+    if (denominator > 0) {
+      length += numerator / denominator;
+    }
+  }
+  console.log(
+    `foot is ${foot} inch ${inch} num ${numerator} and den is ${denominator}`
+  );
+  console.log(length);
+  return length;
+}
+
 function submitRating(e) {
   e.preventDefault();
-  const rating = new FormData(e.target).get("rating");
-  console.log(rating);
-  if (rating === "handRail") {
+  tempChoice = new FormData(e.target).get("rating");
+  console.log(tempChoice);
+  if (tempChoice === "handRail") {
     handRails.classList.remove("no-show");
     document.querySelector("details").removeAttribute("open");
+  } else if (tempChoice === "stairs") {
+    stairSection.classList.remove("no-show");
+    document.querySelector("details").removeAttribute("open");
   }
-  // if (rating) {
-  //   yourRating.innerText = rating;
-  //   console.log(yourRating.innerText);
-  //   mainContainer.classList.add("hidden");
-  //   thankYouContainer.classList.remove("hidden");
-  // }
 }
 class Stairs {
   constructor(dimensionInput, run = 10.5) {
     this.height = dimensionInput;
     this.run = run;
   }
+  stairOl = document.querySelector(".step-hypot");
+
   steps = 0;
   rise = 0;
   stepHypot = 0;
@@ -120,9 +163,53 @@ class Stairs {
 
     console.log(this.stepHypot); //TODO testing
     console.log(this.rise);
+    this.renderStairHypot();
+  }
+  renderStairHypot() {
+    document.querySelector(".stair-answer").textContent = this.rise;
+
+    let nextHypot = this.stepHypot;
+
+    for (let i = 0; i < this.steps; i++) {
+      let stepHypotLI = document.createElement("li");
+      stepHypotLI.textContent = nextHypot.toFixed(3);
+      nextHypot += this.stepHypot;
+      console.log(stepHypotLI.textContent);
+      this.stairOl.append(stepHypotLI);
+    }
   }
 }
-btn.addEventListener("click", evaluateInput);
+class Handrail {
+  constructor(length, picketMaxSpace = 5.5, picketThickness = 1.5) {
+    this.length = length;
+    this.picketMaxSpace = picketMaxSpace;
+    this.picketThickness = picketThickness;
+  }
+  picketList = document.querySelector(".pickets-list");
+
+  pickets = 0;
+  picketSpacing = 0;
+  calculatePickets() {
+    this.pickets = Math.ceil(
+      (this.length - this.picketThickness) / this.picketMaxSpace
+    );
+    this.picketSpacing = (this.length - this.picketThickness) / this.pickets;
+    this.renderPickets();
+  }
+  renderPickets() {
+    console.log(this.pickets);
+    let nextPicket = this.picketSpacing;
+    //this prints out a list of all the pickets dimensions
+    for (let i = 0; i < this.pickets; i++) {
+      let picketLI = document.createElement("li");
+      picketLI.textContent = nextPicket.toFixed(3);
+      nextPicket += this.picketSpacing;
+      console.log(picketLI.textContent); //TODO debugging
+      this.picketList.append(picketLI);
+    }
+  }
+}
+btn.addEventListener("click", inputBreakdown);
 form.addEventListener("submit", submitRating);
 
 //===============Future Idea for drawing========================
