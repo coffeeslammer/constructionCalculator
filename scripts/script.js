c64debb024c600c3f7b8672453cc65c1b3ab9ded
Don Christensen <noobgrammer@gmail.com>
2023-09-23 13:02:44 -0700

    refactored code. Renamed functions. removed old test code and unneeded comments

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,14 @@
 const lengthInput = document.querySelector("#dimension-input");
 // const picketList = document.querySelector(".pickets-list");
 const btn = document.querySelector(".btn");
-const form = document.querySelector(".form-rate");
-const handRails = document.querySelector(".handrail-section");
-const stairSection = document.querySelector(".stairs-section");
+const decidedCalculation = document.querySelector(".chooseCalculation");
+// const handRails = document.querySelector(".handrail-section");
+// const stairSection = document.querySelector(".stairs-section");
 const inputLabel = document.querySelector("#base-input-label"); //FIXME this needs a different name, not using label
 
 let tempChoice; //FIXME this is a temp global to determine what choice was made
 //for now I need it global until I refactor
 
-// function calculatePickets(dimension) {
-//   let picketMaxSpace = 5.5;
-//   let picketThickness = 1.5;
-
-//   let pickets = Math.ceil((dimension - picketThickness) / picketMaxSpace);
-//   renderPickets((dimension - picketThickness) / pickets, pickets);
-// }
 //FIXME give preCheck a better name, doesn't make much sense.
 function preCheck(choice) {
   if (choice.innerHTML !== "") {
@@ -27,28 +20,19 @@ function preCheck(choice) {
     document.querySelector(".invalid").classList.add("hidden");
   }
 }
-// function renderPickets(spacing, picket) {
-//   let nextPicket = spacing;
-//   for (let i = 0; i < picket; i++) {
-//     let picketLI = document.createElement("li");
-//     picketLI.textContent = nextPicket.toFixed(3);
-//     nextPicket += spacing;
-//     console.log(picketLI.textContent);
-//     picketList.append(picketLI);
-//   }
-// }
-function radioChoice(dimensions) {
+
+function compareChoice(dimensions) {
   console.log(tempChoice); //TODO debugging
   if (tempChoice === "stairs") {
     const stair = new Stairs(dimensions);
     preCheck(stair.stairOl);
-    inputLabel.textContent = "Enter overall height";
+    // inputLabel.textContent = "Enter overall height";
     stair.stairMath();
-  } else if (tempChoice === "handRail") {
+  } else if (tempChoice === "handrails") {
     const handrail = new Handrail(dimensions);
     preCheck(handrail.picketList);
 
-    inputLabel.textContent = "Enter length of handrail";
+    // inputLabel.textContent = "Enter length of handrail";
     handrail.calculatePickets();
   } else if (tempChoice === "roof") {
     //do this
@@ -61,41 +45,13 @@ function inputBreakdown() {
     if (isNaN(usableDimension)) {
       document.querySelector(".invalid").classList.remove("hidden");
     } else {
-      radioChoice(+usableDimension);
+      compareChoice(+usableDimension);
     }
   } else {
-    radioChoice(+checkDimension);
+    compareChoice(+checkDimension);
   }
 }
-//FIXME this needs to be set up to not care what choice I do just break input down to usable numbers
-function evaluateInput() {
-  const picketDimension = lengthInput.value;
-  console.log(picketDimension);
-  if (isNaN(picketDimension)) {
-    console.log("is not a number");
-    const railLength = breakingInputDown(picketDimension);
-    if (isNaN(railLength)) {
-      document.querySelector(".invalid").classList.remove("hidden");
-    } else {
-      if (picketList.innerHTML !== "") {
-        picketList.innerHTML = "";
-      }
-      if (!document.querySelector(".invalid").classList.contains("hidden")) {
-        document.querySelector(".invalid").classList.add("hidden");
-      }
-      calculatePickets(railLength);
-    }
-  } else {
-    //Checks to make sure there isn't a list already rendered
-    //If there is then this clears it
-    if (picketList.innerHTML !== "") {
-      picketList.innerHTML = "";
-    }
-    const stair = new Stairs(+picketDimension); //TODO testing
-    stair.stairMath(+picketDimension);
-    calculatePickets(+picketDimension);
-  }
-}
+
 function breakingInputDown(dismantling) {
   let foot = 0;
   let inch = 0;
@@ -126,20 +82,26 @@ function breakingInputDown(dismantling) {
   }
   console.log(
     `foot is ${foot} inch ${inch} num ${numerator} and den is ${denominator}`
-  );
+  ); //TODO debugging
   console.log(length);
   return length;
 }
 
-function submitRating(e) {
+function submittedCalculationChoice(e) {
   e.preventDefault();
-  tempChoice = new FormData(e.target).get("rating");
+  tempChoice = new FormData(e.target).get("calculationChoice");
   console.log(tempChoice);
-  if (tempChoice === "handRail") {
-    handRails.classList.remove("no-show");
+  if (tempChoice === "handrails") {
+    //TODO I may need a generic function here. This could get pretty big
+    //the more calculation options I add in
+    inputLabel.textContent = "Enter length of handrail";
+    document.querySelector(".stairs-section").classList.add("no-show");
+    document.querySelector(".handrail-section").classList.remove("no-show");
     document.querySelector("details").removeAttribute("open");
   } else if (tempChoice === "stairs") {
-    stairSection.classList.remove("no-show");
+    inputLabel.textContent = "Enter overall height";
+    document.querySelector(".handrail-section").classList.add("no-show");
+    document.querySelector(".stairs-section").classList.remove("no-show");
     document.querySelector("details").removeAttribute("open");
   }
 }
@@ -210,25 +172,4 @@ class Handrail {
   }
 }
 btn.addEventListener("click", inputBreakdown);
-form.addEventListener("submit", submitRating);
-
-//===============Future Idea for drawing========================
-// function draw() {
-//   const canvas = document.querySelector("#canvas");
-
-//   if (!canvas.getContext) {
-//     return;
-//   }
-//   const ctx = canvas.getContext("2d");
-
-//   // set line stroke and line width
-//   ctx.strokeStyle = "red";
-//   ctx.lineWidth = 3;
-
-//   // draw a red line
-//   ctx.beginPath();
-//   ctx.moveTo(100, 500);
-//   ctx.lineTo(500, 300);
-//   ctx.stroke();
-// }
-// draw();
+decidedCalculation.addEventListener("submit", submittedCalculationChoice);
