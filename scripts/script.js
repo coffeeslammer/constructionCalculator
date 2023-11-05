35989ef2d31e16a520923b04e1a962bfa7f785cb
Don Christensen <noobgrammer@gmail.com>
2023-11-04 18:57:42 -0700

    trying input instead of click for selection

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
-"use strict";
+import { Stairs } from "./stairs.js";
+import { Handrail } from "./handrail.js";
+// ("use strict");
 
 const lengthInput = document.querySelector(".dimension-input");
 const btn = document.querySelector(".calculate-btn");
@@ -24,7 +26,8 @@ function showHandrail() {
   document.querySelector(".stairs-section").classList.add("no-show");
   document.querySelector(".handrail-section").classList.remove("no-show");
 }
-
+//FIXME I think I need to refactor Input class. I tried to separate it into a separate file but was getting errors.
+//I think it has to do with using the DOM
 class Input {
   constructor() {
     this.handelDimensionInput = this.handelDimensionInput.bind(this);
@@ -149,135 +152,7 @@ class Input {
     }
   }
 }
-class Stairs {
-  // getDimension(dimensionInput) {
-  //   this.#height = dimensionInput;
-  // }
-  stairOl = document.querySelector(".step-hypot");
 
-  #maxRiseHeight = 7.75;
-  #height = 0;
-  #steps = 0;
-  #rise = 0;
-  #run = 10.5;
-  #stepHypot = 0;
-  #headroom = 82;
-  #floor = 10; //these are for future use
-  #treads = 11.5;
-  #treadThickness = 1;
-  #opening = 0;
-  #angle = 0;
-
-  set stairHeight(height) {
-    this.#height = height;
-  }
-  set stairRun(newRun) {
-    this.#run = newRun;
-  }
-  //this is if for some reason you need to override the max rise
-  set riser(newRise) {
-    this.#maxRiseHeight = newRise;
-  }
-  set headRoom(headHeight) {
-    this.#headroom = headHeight;
-  }
-  set floorSize(joist) {
-    this.#floor = joist;
-  }
-  set treadThickness(tread) {
-    this.#treads = tread;
-  }
-  //FIXME this is very vague for a method name. Doesn't say much of what it is doing.
-  stairMath() {
-    this.#steps = Math.ceil(this.#height / this.#maxRiseHeight);
-    this.#rise = this.#height / this.#steps;
-    this.#stepHypot = Math.sqrt(this.#run ** 2 + this.#rise ** 2);
-    this.#angle = +((Math.atan2(this.#rise, this.#run) * 180) / Math.PI).toFixed(3);
-    this.#opening = this.#headroom / Math.tan(Math.atan2(this.#rise, this.#run));
-
-    console.log(this.#stepHypot); //TODO testing
-    console.log(this.#rise);
-    console.log(this.#angle);
-    this.renderStairHypot();
-  }
-  checkOptions() {
-    if (document.querySelector("#chbx-run").checked) {
-      console.log(+document.querySelector("#stair-run-input").value);
-      //FIXME this opens a whole can of worms for more error checking
-      //there could be a dozen inputs that each need to be error checked
-      this.stairRun = +document.querySelector("#stair-run-input").value;
-    } else {
-      this.stairRun = 10.5; //FIXME for now I need this because if you check the box then uncheck it nothing happens
-      //may need a reset option or something
-    }
-    if (document.getElementById("chbx-max-rise-override").checked) {
-      this.riser = +document.getElementById("stair-max-rise-override-input").value;
-    } else {
-      this.riser = 7.75;
-    }
-  }
-  renderStairHypot() {
-    //TODO this I think should be a render class
-    // document.querySelector(".stair-answer").innerHTML = `Your rise is ${this.#rise.toFixed(
-    //   3
-    // )}<br>Steps ${this.#steps}<br>Angle ${this.#angle.toFixed(
-    //   3
-    // )}<br>Step hypot ${this.#stepHypot.toFixed(3)} <br>Min opening is ${this.#opening.toFixed(
-    //   3
-    // )}<br>Stair length is ${this.#run * this.#steps}`;
-    document.querySelector(".stair-answer").innerHTML = `<ul>
-    <li>Your rise is ${this.#rise.toFixed(3)}</li>
-        <li>Steps ${this.#steps}</li>
-        <li>Angle ${this.#angle.toFixed(3)}</li>
-        <li>Step hypot ${this.#stepHypot.toFixed(3)}</li>
-        <li>Min opening is ${this.#opening.toFixed(3)}</li>
-        <li> Stair length is ${this.#run * this.#steps}</li></ul>`;
-
-    let nextHypot = this.#stepHypot;
-
-    for (let i = 0; i < this.#steps; i++) {
-      let stepHypotLI = document.createElement("li");
-      stepHypotLI.textContent = nextHypot.toFixed(3);
-      nextHypot += this.#stepHypot;
-      console.log(stepHypotLI.textContent);
-      this.stairOl.append(stepHypotLI);
-    }
-  }
-}
-class Handrail {
-  constructor(picketMaxSpace = 4, picketThickness = 1.5) {
-    this.picketMaxSpace = picketMaxSpace + picketThickness;
-    this.#picketThickness = picketThickness;
-  }
-  #length = 0;
-  #picketThickness = 1.5;
-  #pickets = 0;
-  #picketSpacing = 0;
-
-  getDimension(length) {
-    this.#length = length;
-  }
-  //TODO create more setting functions for spacing and thickness
-  picketList = document.querySelector(".pickets-list");
-
-  calculatePickets() {
-    this.#pickets = Math.ceil((this.#length - this.#picketThickness) / this.picketMaxSpace);
-    this.#picketSpacing = (this.#length - this.#picketThickness) / this.#pickets;
-    this.renderPickets();
-  }
-  renderPickets() {
-    console.log(this.#pickets);
-    let nextPicket = this.#picketSpacing;
-    //this prints out a list of all the pickets dimensions
-    for (let i = 0; i < this.#pickets; i++) {
-      let picketLI = document.createElement("li");
-      picketLI.textContent = nextPicket.toFixed(3);
-      nextPicket += this.#picketSpacing;
-      console.log(picketLI.textContent); //TODO debugging
-      this.picketList.append(picketLI);
-    }
-  }
-}
 const input = new Input();
 const stair = new Stairs();
 const handrail = new Handrail();
@@ -296,4 +171,4 @@ btn.addEventListener("click", () => {
   }
 });
 
-selection.addEventListener("click", input.choiceSelected);
+selection.addEventListener("input", input.choiceSelected);
